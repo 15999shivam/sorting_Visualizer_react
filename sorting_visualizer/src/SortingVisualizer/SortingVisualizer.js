15ac7c9c932cad7b806ef18bb5c3030604493b4c
Shivam <shivamgarg260@gmail.com>
2019-10-16 13:48:46 +0530

    made two inputs for bars and time to give more controll

--- sorting_visualizer/src/SortingVisualizer/SortingVisualizer.js
+++ sorting_visualizer/src/SortingVisualizer/SortingVisualizer.js
@@ -6,7 +6,9 @@ export default class SortingVisualizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      array: []
+      array: [],
+      numberOfBars: 40,
+      timeInMS: 10
     };
   }
 
@@ -16,7 +18,7 @@ export default class SortingVisualizer extends Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < this.state.numberOfBars; i++) {
       array.push(getRandomNumber(5, 1000));
     }
     this.setState({ array });
@@ -28,7 +30,7 @@ export default class SortingVisualizer extends Component {
     // console.log(animations);
     const newAnimations = [];
     for (const animation of animations) {
-      console.log(animation.swap);
+      // console.log(animation.swap);
       newAnimations.push(animation.comparison);
       newAnimations.push(animation.comparison);
       newAnimations.push(animation.swap);
@@ -45,38 +47,86 @@ export default class SortingVisualizer extends Component {
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
-        }, i * 2);
+        }, i * this.state.timeInMS);
       } else {
         setTimeout(() => {
           const [barOneIdx, newHeight] = newAnimations[i];
           // console.log(newHeight);
           const barOneStyle = arrayBars[barOneIdx].style;
           barOneStyle.height = `${newHeight / 15}vh`;
-        }, i * 2);
+        }, i * this.state.timeInMS);
       }
     }
   }
   quickSort() {}
   heapSort() {}
   bubbleSort() {}
+
+  // TODO:modify accc to usecase
   testAlgorithms() {
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 100; i++) {
       const array = [];
       const length = getRandomNumber(1, 1000);
       for (let j = 0; j < length; j++) {
         array.push(getRandomNumber(-1000, 1000));
       }
       const javaScriptSortArray = array.slice().sort((a, b) => a - b);
-      const mergeSortArray = algo.mergeSort(array.slice());
+      algo.mergeSort(array);
 
-      console.log(areArrayEqual(javaScriptSortArray, mergeSortArray));
+      console.log(areArrayEqual(javaScriptSortArray, array));
     }
+  }
+
+  changeBars() {
+    const input = this.refs.barsInput;
+    const value = input.value;
+    const cb = () => {
+      this.resetArray();
+    };
+    if (value > 300) {
+      this.setState(
+        {
+          numberOfBars: 300
+        },
+        cb
+      );
+    } else {
+      if (value < 0) {
+        this.setState(
+          {
+            numberOfBars: 0
+          },
+          cb
+        );
+      } else {
+        console.log("hii");
+        this.setState(
+          {
+            numberOfBars: value
+          },
+          cb
+        );
+      }
+    }
+  }
+
+  changeTime() {
+    const input = this.refs.timeInput;
+    const value = input.value;
+    if (value < 0) {
+      return this.setState({
+        timeInMS: 0
+      });
+    }
+    this.setState({
+      timeInMS: value
+    });
   }
 
   render() {
     const { array } = this.state;
     return (
-      <div>
+      <div className='container'>
         <div className='array-container'>
           {array.map((value, index) => {
             return (
@@ -88,12 +138,31 @@ export default class SortingVisualizer extends Component {
             );
           })}
         </div>
-        <button onClick={() => this.resetArray()}>Generate new Array</button>
-        <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        <button onClick={() => this.quickSort()}>Quick Sort</button>
-        <button onClick={() => this.heapSort()}>Heap Sort</button>
-        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-        <button onClick={() => this.testAlgorithms()}>Test algorithms</button>
+        <div className='buttons'>
+          <button onClick={() => this.resetArray()}>Generate new Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button onClick={() => this.quickSort()}>Quick Sort</button>
+          <button onClick={() => this.heapSort()}>Heap Sort</button>
+          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <button onClick={() => this.testAlgorithms()}>Test algorithms</button>
+        </div>
+        <div className='inputs'>
+          <label>Number Of Bars( 0 - 300 ): </label>
+          <input
+            type='number'
+            onChange={e => this.changeBars()}
+            ref='barsInput'
+            value={this.state.numberOfBars}
+          />
+          <br />
+          <label>Time in ms( >0 ): </label>
+          <input
+            type='number'
+            onChange={e => this.changeTime()}
+            ref='timeInput'
+            value={this.state.timeInMS}
+          />
+        </div>
       </div>
     );
   }
